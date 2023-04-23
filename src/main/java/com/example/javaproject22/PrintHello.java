5e22d30a87e59a995eb49e45dd90519a581e90a6
BlackHan26 <128695414+BlackHan26@users.noreply.github.com>
2023-04-23 12:59:42 +0900

    feat: first commit -java Project 빌드

---
+++ src/main/java/com/example/javaproject22/PrintHello.java
@@ -0,0 +1,7 @@
+package com.example.javaproject22;
+
+public class PrintHello {
+    public void print(){
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/example/javaproject22/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject22;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();//인스턴트화
+        printHello.print();
+    }
+}
