534c5a63f741fb9f7b6cbb4094991958c4b3c28d
BlackHan26 <128695414+BlackHan26@users.noreply.github.com>
2023-04-23 23:25:50 +0900

    feat: Bye 기능 추가 -ByeClass , ByeClassTest 추가

---
+++ src/main/java/com/example/javaproject22/CrawlDate.java
@@ -0,0 +1,11 @@
+package com.example.javaproject22;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime laseCrawlDateTime){
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
